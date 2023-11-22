a333e5d37fad632344556057f86a83dc9642a661
dududu <286454048@qq.com>
2023-11-22 22:55:50 +0800

    首次提交

---
+++ src/main/java/com/dudu/dududu/DududuApplication.java
@@ -0,0 +1,14 @@
+package com.dudu.dududu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DududuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DududuApplication.class, args);
+    }
+
+}
